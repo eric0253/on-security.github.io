ccbd7499191b884f28971ed869c9e1fcc9c80c60
Eric Bochiccia <77254171+eric0253@users.noreply.github.com>
2023-12-04 21:47:55 -0300

    Update employee_form.php

--- assets/php/employee_form.php
+++ assets/php/employee_form.php
@@ -1,6 +1,6 @@
 <?php
 /*Criando conexão com o banco de dados */
-$servidor = 'localhost';
+$servidor = 'rafael.mysql.database.azure.com';
 $user = 'rafael';
 $password = 'Felippe@4';
 $banco = 'programa';
